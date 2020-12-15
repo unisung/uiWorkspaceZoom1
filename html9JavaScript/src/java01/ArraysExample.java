f20e6c961b051262c431001738e9e8daefd9678c
unisung <vctor@naver.com>
2020-12-15 16:58:14 +0900

    12-15-03

---
+++ html9JavaScript/src/java01/ArraysExample.java
@@ -0,0 +1,12 @@
+package java01;
+
+import java.util.Date;
+public class ArraysExample {
+
+	public static void main(String[] args) {
+		Object[] arr= {new Person(),new String(),new Integer(10), new Double(1.0),new Date()};
+
+	}
+
+}
+class Person extends Object{}
